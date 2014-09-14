74bcf0df5b92dbc82930d1b7e9f7d46743ff2dbf
Kate Catlin <kate.catlin@detroitlabs.com>
2014-09-13 23:09:18 -0400

    Creating a method for main menu

--- MyCalculator/src/com/mycalculator/java/MyCalculator.java
+++ MyCalculator/src/com/mycalculator/java/MyCalculator.java
@@ -4,13 +4,50 @@ package com.mycalculator.java;
  * Created by katecatlin on 9/13/14.
  */
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 import  java.util.Scanner;
 
 public class MyCalculator {
 
-    public static void main(String[] args) {
 
+    public static void methodPrintOpening() {
         Scanner in = new Scanner(System.in);
 
+        System.out.println("What kind of function would you like to perform? \n Press ONLY the number:");
+        System.out.println("'1' for addition");
+        System.out.println("'2' for subtraction");
+        System.out.println("'3' for multiplication");
+        System.out.println("'4' for division");
+        System.out.println("'5' to find a square root");
+
+        if (in.hasNextInt()) {
+            System.out.println("it's an int!");
+            int yourChoice = in.nextInt();
+            System.out.println(yourChoice);
+        } else {
+            System.out.println("Sorry, that's not an option. Please choose a number between 1 and 5, and type only the number! \n");
+        MyCalculator.methodPrintOpening();}
     }
-}
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        boolean KeepGoing = true;
+        int i;
+        int yourChoice = 0;
+
+        while (KeepGoing) {
+            System.out.println("Entered the while loop");
+            MyCalculator.methodPrintOpening();
+
+            System.out.println("Still in the loop");
+
+            KeepGoing = false;
+
+            }
+        System.out.println("Thanks for using this calculator!");
+        }
+    }
