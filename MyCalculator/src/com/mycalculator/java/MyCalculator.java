625280a73e4293a9830bd578111fb3093b5173fe
Kate Catlin <kate.catlin@detroitlabs.com>
2014-09-14 23:41:52 -0400

    Now working to operate multiple times

--- MyCalculator/src/com/mycalculator/java/MyCalculator.java
+++ MyCalculator/src/com/mycalculator/java/MyCalculator.java
@@ -6,64 +6,82 @@ package com.mycalculator.java;
 
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 
-//now let's see if we can operate on previous answers. 
+//My big goal here was to find a way to continue operating on a number one already going, not just start fresh each time.
+//My secondary goal here was to figure out methods, which was mildly successful. Still confused on static vs. nonstatic, etc
+//hope I used most of them right but got the right answeres sooooooooooo. yeah.
 
 import  java.util.Scanner;
 
 public class MyCalculator {
 
     public static int yourChoice;
-    public static double num1, num2;
-    public static boolean keepGoing = true;
+    public static int saveLatestSolutionq = 1;
+    public static double num2, latestSolution;
+    public static double num1 = 987654321;
+    public static boolean keepGoing = true, savingLatestSolution = true;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-
-
         while (keepGoing) {
-            //System.out.println("Entered the while loop");
-            yourChoice = MyCalculator.getYourChoice();
-            //System.out.println("yourChoice before cases is: " +yourChoice);
-            switch (yourChoice) {
-                case 1:
-                    System.out.println("You chose addition!");
-                    System.out.println("please enter a number to add:");
-                    num1 = in.nextDouble();
-                    System.out.println("Please enter a second number to add:");
-                    num2 = in.nextDouble();
-                    System.out.println("The sum is: " + sum(num1, num2));
-                    break;
-                case 2:
-                    System.out.println("You chose subtraction!");
-                    System.out.println("please enter a number:");
-                    num1 = in.nextDouble();
-                    System.out.println("Please enter the number to subtract:");
-                    num2 = in.nextDouble();
-                    System.out.println("The answer is: " + subtract(num1, num2));
-                    break;
-                case 3:
-                    System.out.println("You chose multiplication!");
-                    System.out.println("please enter a number:");
-                    num1 = in.nextDouble();
-                    System.out.println("Please enter a second number:");
-                    num2 = in.nextDouble();
-                    System.out.println("The answer is: " + multiply(num1, num2));
-                    break;
-                case 4:
-                    System.out.println("You chose division!");
-                    System.out.println("please enter a number:");
-                    num1 = in.nextDouble();
-                    System.out.println("Please enter a second number:");
-                    num2 = in.nextDouble();
-                    System.out.println("The quotient is: " + divide(num1, num2));
-                    break;
-                case 5:
-                    System.out.println("You chose square root!");
-                    System.out.println("please enter a number:");
-                    num1 = in.nextDouble();
-                    System.out.println("The square root is: " + squareroot(num1));
-                    break;
+            while (savingLatestSolution) {
+                //System.out.println("Entered the while loop");
+                yourChoice = MyCalculator.getYourChoice();
+                //System.out.println("yourChoice before cases is: " +yourChoice);
+                switch (yourChoice) {
+                    case 1:
+                        //System.out.println("You chose addition!");
+                        if (num1 == 987654321) {
+                            num1 = getNum1();
+                        } else num1 = latestSolution;
+                        num2 = getNum2();
+                        System.out.println(num1 + "+" + num2);
+                        System.out.println("The sum is: " + sum(num1, num2));
+                        latestSolution = sum(num1, num2);
+                        MyCalculator.saveLatestSolution();
+                        break;
+                    case 2:
+                        //System.out.println("You chose subtraction!");
+                        //System.out.println("The latest solutions is " + latestSolution);
+                        if (num1 == 987654321) {
+                            num1 = getNum1();
+                        }
+                        num2 = getNum2();
+                        System.out.println(num1 + "-" + num2);
+                        System.out.println("The answer is: " + subtract(num1, num2));
+                        latestSolution = subtract(num1, num2);
+                        MyCalculator.saveLatestSolution();
+                        break;
+                    case 3:
+                        //System.out.println("You chose multiplication!");
+                        if (num1 == 987654321) {
+                            num1 = getNum1();
+                        } else num1 = latestSolution;
+                        num2 = getNum2();
+                        System.out.println("The answer is: " + multiply(num1, num2));
+                        latestSolution = multiply(num1, num2);
+                        MyCalculator.saveLatestSolution();
+                        break;
+                    case 4:
+                        //System.out.println("You chose division!");
+                        if (num1 == 987654321) {
+                            num1 = getNum1();
+                        } else num1 = latestSolution;
+                        num2 = getNum2();
+                        System.out.println("The quotient is: " + divide(num1, num2));
+                        latestSolution = divide(num1, num2);
+                        MyCalculator.saveLatestSolution();
+                        break;
+                    case 5:
+                        //System.out.println("You chose square root!");
+                        if (num1 == 987654321) {
+                            num1 = getNum1();
+                        } else num1 = latestSolution;
+                        System.out.println("The square root is: " + squareroot(num1));
+                        latestSolution = squareroot(num1);
+                        MyCalculator.saveLatestSolution();
+                        break;
+                }
             }
 
             MyCalculator.keepGoingQuestion();
@@ -71,9 +89,67 @@ public class MyCalculator {
         System.out.println("\nThanks for using this calculator!");}
 
 
+    public static boolean checkIfDouble (Scanner input) {
+        if (input.hasNextDouble()) {
+            //System.out.println("Went through checkIfInteger.");
+            return true;
+        }
+        else return false;
+    }
+
+    public static boolean checkIfInRange(int yourChoice) {
+        //System.out.println("Went to checkIfInRange.");
+        if (yourChoice<=5 && yourChoice>=1) {
+            //System.out.println("Passed checkIfInteger.");
+            return true;
+        }
+        else return false;
+    }
+
+    public static boolean checkIfInteger(Scanner input) {
+        if (input.hasNextInt ()) {
+            //System.out.println("Went through checkIfInteger.");
+            return true;
+        }
+        else return false;
+    }
+
+    public static double divide(double num1, double num2) {
+        return num1/num2;
+    }
+
+    public static double getNum1 () {
+        Scanner in = new Scanner(System.in);
+        System.out.println("please enter your first number:");
+        //System.out.println("you entered " + num1);
+        if (checkIfDouble(in)) {
+            num1 = in.nextDouble();
+            //System.out.println("Went through hasNextDouble.");
+        } else {
+            System.out.println("Sorry that's not an option! Please enter a number: ");
+            MyCalculator.getNum1();
+        }
+
+        return num1;
+    }
+
+    public static double getNum2 () {
+        Scanner in = new Scanner(System.in);
+        System.out.println("please enter a second number:");
+        //System.out.println("you entered " + num1);
+        if (checkIfDouble(in)) {
+            num2 = in.nextDouble();
+            //System.out.println("Went through hasNextDouble.");
+        } else {
+            System.out.println("Sorry that's not an option! Please enter a number: ");
+            MyCalculator.getNum2();
+        }
+
+        return num2;
+    }
+
     public static int getYourChoice() {
         Scanner in = new Scanner(System.in);
-        int yourChoice=34829;
 
         System.out.println("What kind of function would you like to perform? \n Press ONLY the number:");
         System.out.println("'1' for addition");
@@ -102,55 +178,53 @@ public class MyCalculator {
         return yourChoice;
     }
 
-    public static double sum(double num1, double num2) {
-        return num1 + num2;
-    }
-
-    public static double subtract(double num1, double num2) {
-        return num1 - num2;
-    }
-    public static double multiply(double num1, double num2) {
-        return num1*num2;
-    }
-    public static double divide(double num1, double num2) {
-        return num1/num2;
-    }
-    public static double squareroot(double num1) {
-        return Math.sqrt(num1);
-    }
-
     public static int keepGoingQuestion() {
         System.out.println("\nWould you like to ask another question?\n");
-        System.out.println("TO QUIT, press 1. otherwise press any other number.\n");
+        System.out.println("TO QUIT, press 1. otherwise press any other key!\n");
         Scanner in = new Scanner(System.in);
         if (checkIfInteger(in)) {
             int keepGoingQ = in.nextInt();
             if (keepGoingQ == 1) keepGoing = false;
-        return 0;}
-        else {
-            System.out.println("Sorry, you have to enter a number for me to understand! \n");
-            MyCalculator.keepGoingQuestion();
+            return 0;}
+        else keepGoing = false;
+        return 0;
+    }
 
+    public static double multiply(double num1, double num2) {
+        return num1*num2;
+    }
+
+    public static int saveLatestSolution () {
+        System.out.println("\nWould you like to perform another operation on " + latestSolution + "?\n");
+        System.out.println("If so, press 1. Otherwise, press any other key!\n");
+        Scanner in = new Scanner(System.in);
+        if (checkIfInteger(in)) {
+            int saveLatestSolutionq = in.nextInt();
+            if (saveLatestSolutionq == 1) {
+                num1 = latestSolution;
+
+            } else {
+                System.out.println("ok, we'll start fresh!");
+                savingLatestSolution = false;
+            }
+            }
+        else {
+            System.out.println("ok, we'll start fresh!");
+            savingLatestSolution = false;
         }
         return 0;
     }
 
-
-    public static boolean checkIfInteger(Scanner input) {
-        if (input.hasNextInt ()) {
-            //System.out.println("Went through checkIfInteger.");
-            return true;
-        }
-        else return false;
+    public static double squareroot(double num1) {
+        return Math.sqrt(num1);
     }
 
-    public static boolean checkIfInRange(int yourChoice) {
-        //System.out.println("Went to checkIfInRange.");
-        if (yourChoice<=5 && yourChoice>=1) {
-            //System.out.println("Passed checkIfInteger.");
-            return true;
-        }
-        else return false;
+    public static double subtract(double num1, double num2) {
+        return num1-num2;
+    }
+
+    public static double sum(double num1, double num2) {
+        return num1 + num2;
     }
 
 }
